11fce629542030da0232f165469b4022dfa5cbf4
Manuel Riezebosch <mriezebosch@gmail.com>
2017-07-07 14:23:05 +0200

    thread synchronization met sleeps....

---
+++ cnetprog/SynchronizationDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace cnetprog
+{
+    public class SynchronizationDemo
+    {
+        Action Done;
+
+        [Fact]
+        public void DezeTestMoetWachtenTotErErgensAndersWatGebeurdIs()
+        {
+            bool uitgevoerd = false;
+            Done += () => uitgevoerd = true;
+
+            GaIetsUitvoerenFireAndForgetMaarRaiseEventWanneerKlaar();
+            Thread.Sleep(1000);
+
+            Assert.True(uitgevoerd);
+        }
+
+        private void GaIetsUitvoerenFireAndForgetMaarRaiseEventWanneerKlaar()
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(500);
+                Done?.Invoke();
+            });
+        }
+    }
+}
